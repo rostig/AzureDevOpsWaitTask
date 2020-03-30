5a7c151c683631bde987d0232d80a01c5107e4f7
cellcom_nt\rostisge <rostig@gmail.com>
2020-03-30 10:54:10 +0300

    Removed Timeout field. This field is supported by AzureDevOps platform for all tasks out of the box.

--- index.js
+++ index.js
@@ -3,35 +3,17 @@ console.log("Starting waiting script");
 
 const tl = require("azure-pipelines-task-lib/task");
 const request = require('request-promise-native');
-const moment = require('moment');
-
-function isValidTimer(timeout, startingPoint, interval) {
-    if (timeout <= 0) return true;
-
-    const currentTimePoint = new moment();
-    const duration = moment.duration(currentTimePoint.diff(startingPoint)).asMinutes();
-    if (duration >= timeout) {
-        clearInterval(interval);
-        tl.setResult(tl.TaskResult.Failed, 'Defined timeout elapsed');
-        return false;
-    }
-    return true;
-}
 
 async function run() {
-    const startingPoint = new moment();
 
     try {
         const url = tl.getVariable("SYSTEM_TEAMFOUNDATIONSERVERURI") + tl.getVariable("SYSTEM_TEAMPROJECTID") + "/_apis/Release/releases/" + tl.getVariable("RELEASE_RELEASEID") + "?api-version=5.0";
         const variableName = tl.getInput('variableName', true);
         const succeedValue = tl.getInput('succeedValue', true);
         const failedValue = tl.getInput('failedValue', false);
-        const timeout = tl.getInput('Timeout', true)*1;
         let failedRequestNumber = 0;
 
         const interval = setInterval(async () => {
-            if (!isValidTimer(timeout, startingPoint, interval)) return;
-
             let releaseJson;
             try {
                 releaseJson = JSON.parse(await request({
